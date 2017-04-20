62914343b3f4b6b8bd220daad37fb94448c61fa0
Robert Van Voorhees <rcvanvo@gmail.com>
2017-04-20 17:02:02 -0400

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+
+FROM fedora:latest
+
+RUN dnf install -y createrepo python2-boto3 python-pexpect deltarpm python-deltarpm git && \
+  dnf clean all
+
+RUN git clone https://github.com/SMARTRACTECHNOLOGY/rpm-s3.git --recurse-submodules /rpm-s3 && \
+  chmod +x /rpm-s3/bin/rpm-s3 && \
+  ln -sf /rpm-s3/bin/rpm-s3 /usr/local/bin/rpm-s3
+
+  ENTRYPOINT ["/usr/local/bin/rpm-s3"]
---
+++ add-rpm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ -d "$(pwd)/target" ]]; then
+  BUILDFOLDER=$(pwd)/target
+else
+  BUILDFOLDER=$(pwd)
+fi
+
+ARCHS="x86_64"
+
+ENV=$2
+
+shift;
+
+VERSION="${VERSION:-latest}"
+
+MYUID="${USERID:-$(id -u)}"
+MYGID="${GID:-$(id -g)}"
+
+for arch in ${ARCHS}; do
+  docker run -t --rm -u ${MYUID}:${MYGID} -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+      -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -v ${BUILDFOLDER}:/data -w /data \
+      --privileged \
+      smartcosmos/rpm-s3:${VERSION} -v -b repo.lifecycles.io --region "us-east-1" --visibility "public-read" -p "linux/gateway/rpm/${ENV}/${arch}" $@
+done
