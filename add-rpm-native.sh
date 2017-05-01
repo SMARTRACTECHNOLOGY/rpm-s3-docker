6ea706de037bbe9e3e173951772e5c7f67331ce3
Robert Van Voorhees <rcvanvo@gmail.com>
2017-05-01 16:18:04 -0400

    Fix native script

--- add-rpm-native.sh
+++ add-rpm-native.sh
@@ -3,8 +3,17 @@ set -e
 
 ARCHS="x86_64"
 
+ACL="public-read"
+REGION="us-east-1"
+REPO="repo.lifecycles.io"
+
 ENV=$1
+
+if [[ "$ENV" != "beta" && "$ENV" != "base" ]]; then
+  echo "Only beta and base are valid options."
+  exit 1
+fi
 
 shift;
 
-~/git/rpm-s3/bin/rpm-s3 -v -b repo.lifecycles.io --region "us-east-1" --visibility "public-read" -p "linux/gateway/rpm/beta/x86_64" $@
+~/git/rpm-s3/bin/rpm-s3 -v -b ${REPO} --region ${REGION} --visibility ${ACL} -p "linux/gateway/rpm/${ENV}/${ARCHS}" $@
